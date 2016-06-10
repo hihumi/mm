9ce64627d0d7b00f0c2f4ba66266d4ea90f166c9
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-11 08:23:10 +0900

    first commit

---
+++ mm/cd.go
@@ -0,0 +1,27 @@
+package mylib
+
+import (
+	"fmt"
+	_ "os"
+)
+
+type CdStruct struct {
+	cdString interface{}
+	cd       interface{}
+}
+
+func CdStringFunc() interface{} {
+	var Cd CdStruct
+	Cd.cdString = "cd"
+
+	return Cd.cdString
+}
+
+func CdPrintFunc() {
+	var Cd CdStruct
+	Cd.cd = CdStringFunc()
+
+	fmt.Printf("%v: カレントディレクトリを変更します\n詳細: オンラインマニュアル: $ man %v\n", Cd.cd, Cd.cd)
+
+	return
+}
