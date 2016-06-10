e2148dfd9104b29c0dfa1c5581d816b2f6fc6939
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-11 08:13:31 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	_ "./mylib"
+	"fmt"
+	_ "os"
+)
+
+func main() {
+	fmt.Println("Unixコマンドの概要を表示します。コマンド名を入力してください。(終了するには、q、またはQキーを入力します)")
+
+	for {
+		fmt.Printf(">>> ")
+		var name string
+		fmt.Scanln(&name)
+
+		q := "q"
+		Q := "Q"
+		if q == name || Q == name {
+			fmt.Println("終了")
+			break
+		}
+
+		SwitchNameFunc(name)
+	}
+}
