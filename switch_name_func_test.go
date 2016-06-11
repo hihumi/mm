a60e6ca09b8e7d7292a9e92a67d60d17191283b7
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-06-11 09:57:56 +0900

    fix

--- switch_name_func_test.go
+++ switch_name_func_test.go
@@ -7,14 +7,14 @@ import (
 
 func TestSwitchNameFuncn(t *testing.T) {
 	expectCd := "cd"
-	actualCd := mylib.CdStringFunc()
+	actualCd := mm.CdStringFunc()
 
 	if expectCd != actualCd {
 		t.Errorf("%s != %s\n", expectCd, actualCd)
 	}
 
 	expectPwd := "pwd"
-	actualPwd := mylib.PwdStringFunc()
+	actualPwd := mm.PwdStringFunc()
 
 	if expectPwd != actualPwd {
 		t.Errorf("%s != %s\n", expectPwd, actualPwd)
